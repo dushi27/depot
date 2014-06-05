4752d66b0820cd4a177236e9725c2e34fc8115f2
Dushyanthi <dushi27@gmail.com>
2014-06-05 17:22:54 +0000

    Error handling for invalid cart params

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,6 +10,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, notice: 'Invalid cart'
+      else
+        respond_to do |format|
+        format.html # show.html.erb
+        format.xml { render xml: @cart }
+      end
+    end
   end
 
   # GET /carts/new
