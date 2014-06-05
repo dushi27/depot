711747049b6ec78f75d07fc3ef3f03ad58d06c4a
Dushyanthi <dushi27@gmail.com>
2014-06-05 05:50:11 +0000

    Add Store controller tests

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a' , minimum: 4
+    asser_select '#main .entry', 3
+    asser_select 'h3', 'Programming Ruby 1.9'
+    asser_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
