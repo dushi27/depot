3a69f0419687d65b0b0b0fd167ea42724f2bec12
Dushyanthi <dushi27@gmail.com>
2014-06-06 14:46:53 +0000

    Create ordering process

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,15 +28,17 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-    @order = Order.new(order_params)
-
-    respond_to do |format|
-      if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
-      else
-        format.html { render :new }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+    @order = Order.new(params[:order])
+    @order.add_line_items_from_cart(current_cart)
+      respond_to do |format|
+        if @order.save
+          Cart.destroy(session[:cart_id])
+          session[:cart_id] = nil
+          format.html { redirect_to(store_url, notice: 'Thank you for your order.' ) }
+          format.xml { render :xml => @order, :status => :created, location: @order }
+        else
+          format.html { render action: "new" }
+          format.xml { render xml: @order.errors, status: :unprocessable_entity }
       end
     end
   end
