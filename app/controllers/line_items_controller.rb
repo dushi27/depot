942b12e34bd4da1721a486a5fa9d12fbbdc8d1d5
Dushyanthi <dushi27@gmail.com>
2014-06-06 15:11:29 +0000

    Create atom feed for products

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -64,7 +64,15 @@ class LineItemsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
+  def who_bought
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.atom
+      format.xml { render xml: @product }
+    end
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
