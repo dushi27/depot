4d2f6e1dd4eedc1d9802d7d5b3d4bdb06a2c2461
Dushyanthi <dushi27@gmail.com>
2014-06-06 18:13:02 +0000

    Update the uesr tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   setup do
-    @user = users(:one)
+    @input_attributes = {
+       name: "sam",
+       password:"private",
   end
 
   test "should get index" do
@@ -18,10 +20,9 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { hashed_password: @user.hashed_password, name: @user.name, salt: @user.salt }
+      post :create, user: @input_attributes
     end
-
-    assert_redirected_to user_path(assigns(:user))
+    assert_redirected_to users_path
   end
 
   test "should show user" do
@@ -35,8 +36,8 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { hashed_password: @user.hashed_password, name: @user.name, salt: @user.salt }
-    assert_redirected_to user_path(assigns(:user))
+    put :update, id: @user.to_param, user: @input_attributes
+    assert_redirected_to users_path
   end
 
   test "should destroy user" do
