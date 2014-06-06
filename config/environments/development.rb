10d6d227376a55655bbf6e648430eaf295dfc7c6
Dushyanthi <dushi27@gmail.com>
2014-06-06 16:08:35 +0000

    Adjust the config settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,8 +35,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  #disable mail delievery in the development 
-  Depot::Application.configure do
-   config.action_mailer.delivery_method = :test
-  end
 end
