48d27d60bf1ee9fba736f3454b3d51df0d1b5309
Dushyanthi <dushi27@gmail.com>
2014-06-07 02:13:04 +0000

    Config for internationalization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,7 +58,14 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
+    @user = User.find(params[:id])
+    begin
+      @user.destroy
+      flash[:notice] = "User #{@user.name} deleted"
+      rescue Exception => e
+        flash[:notice] = e.message
+      end
+    
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
