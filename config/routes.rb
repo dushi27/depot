3e7e4446c92d1961eb7aa0d0590ae6d10f5c8588
dushi27 <dushi27@gmail.com>
2014-08-16 13:21:22 +0000

    add json API for admin userss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Depot::Application.routes.draw do
+  namespace :api, defaults: {format: 'json'}  do
+    namespace :v1 do
+      resources :products
+    end
+  end
+  
   get 'admin' => 'admin#index'
   controller :sessions do
     get 'login' => :new
