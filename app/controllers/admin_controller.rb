74b99803c211079a8ff0d117307b4a14ec95d0c7
dushi27 <dushi27@gmail.com>
2014-09-07 18:15:13 +0000

    add an orders by day chart

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,8 @@
 class AdminController < ApplicationController
   def index
     @total_orders = Order.count
+    @ordersByDate = Order.where('created_at >= :three_weeks_ago', :three_weeks_ago => 3.weeks.ago.at_midnight).group("Date(created_at)").count
+    @data = Array.new()
+    @ordersByDate.each {|key,value| @data << value}
   end
 end
