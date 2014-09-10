da4173de255a17784424b51a555c5ab555fa4f74
dushi27 <dushi27@gmail.com>
2014-09-10 16:45:07 +0000

    patch for startdate bug

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,7 +5,7 @@ class AdminController < ApplicationController
     @total_orders = Order.count
     @ordersByDate = Order.where('created_at >= :three_weeks_ago', :three_weeks_ago => 3.weeks.ago.at_midnight).group("Date(created_at)").count
     #@startDate = @ordersByDate.keys[0]
-    @startDate = Time.parse(@ordersByDate.keys[0]).utc.to_i*1000
+    @startDate = Time.parse(@ordersByDate.keys[0].to_s).utc.to_i*1000
     @data = Array.new()
     @ordersByDate.each {|key,value| @data << value}
   end
