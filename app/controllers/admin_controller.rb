a89fd2cf8b9329809b3e5213943fa797c055a83b
dushi27 <dushi27@gmail.com>
2014-09-10 04:45:36 +0000

    product stats start date bug fix 2

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,7 @@ class AdminController < ApplicationController
   def index
     @total_orders = Order.count
     @ordersByDate = Order.where('created_at >= :three_weeks_ago', :three_weeks_ago => 3.weeks.ago.at_midnight).group("Date(created_at)").count
-    @startDate = @ordersByDate.keys[0]
+    @startDate = Time.parse(@ordersByDate.keys[0]).utc.to_i*1000
     @data = Array.new()
     @ordersByDate.each {|key,value| @data << value}
   end
