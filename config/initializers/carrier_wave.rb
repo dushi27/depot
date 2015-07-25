0cd79b71c8b19df204e6a73326b8589ce05d242b
Dushyanthi Pieris <dushi27@gmail.com>
2015-07-25 17:31:25 +0000

    update CW init

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       provider:              'AWS',                       
       aws_access_key_id:     ENV['S3_KEY'],                      
       aws_secret_access_key: ENV['S3_SECRET'],
-      region: 'Oregon'
+      region: 'us-west-2'
     }
     config.fog_directory  = ENV['S3_BUCKET']                      
   end
