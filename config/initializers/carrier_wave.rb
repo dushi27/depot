9de75312af9770ed5631f220503c4d9beb498991
Dushyanthi Pieris <dushi27@gmail.com>
2015-07-26 21:15:26 +0000

    front end upadtes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,14 +1,15 @@
 if Rails.env.production?
   puts "start running Carrier wave init............................................................"
-  CarrierWave.configure do |config|                  
-    config.fog_credentials = {
-      provider:              'AWS',                       
-      aws_access_key_id:     ENV['S3_KEY'],                      
-      aws_secret_access_key: ENV['S3_SECRET'],
-      region: 'us-west-2'
-    }
-    config.fog_directory  = ENV['S3_BUCKET']                      
-  end
+    CarrierWave.configure do |config|
+        config.fog_credentials = {
+        # Configuration for Amazon S3
+        #user name: railsbaseimagehandler
+          :provider              => 'AWS',
+          :aws_access_key_id     => 'AKIAIFGNK4GWMQBYVZUA',
+          :aws_secret_access_key => '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'
+        }
+        config.fog_directory     =  "railsbaseimagesdev"
+    end
   puts "END running Carrier wave init............................................................"
 end
 
